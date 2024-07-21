fa677d9ca56f420abade733beaa3f595fa94b49f
Yasmine <yasmine.chenafa@gmail.com>
2024-07-21 10:30:04 +0100

    fix bugs

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
         'views/res_partner_views.xml',
         'views/res_company_views.xml',
         'views/subscription_contracts_views.xml',
-        'views/account_move.xml'
+        'views/account_move.xml',
         'reports/account_move_report.xml',
         'reports/sale_order_report.xml',
         'reports/stock_picking_report.xml'
@@ -17,10 +17,10 @@
     'installable': True,
     'application': False,
     'auto_install': False,
-    'assets': {
-        'web.assets_backend': [
-        ],
-    },
-    'qweb': [
-    ],
+    # 'assets': {
+    #     'web.assets_backend': [
+    #     ],
+    # },
+    # 'qweb': [
+    # ],
 }
