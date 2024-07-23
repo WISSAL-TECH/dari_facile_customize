572a43aba29cdc78ffded4c62bdeb42a5f8556b2
Yasmine <yasmine.chenafa@gmail.com>
2024-07-23 11:24:11 +0100

    edit report

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'summary': 'Add custom file to Odoo',
     'description': 'A module to add a custom files to Odoo.',
-    'depends': ['base', 'sale','sales_contract_and_recurring_invoices'],
+    'depends': ['base','stock', 'sale','sales_contract_and_recurring_invoices'],
     'data': [
         'views/res_partner_views.xml',
         'views/res_company_views.xml',
@@ -18,10 +18,5 @@
     'installable': True,
     'application': False,
     'auto_install': False,
-    # 'assets': {
-    #     'web.assets_backend': [
-    #     ],
-    # },
-    # 'qweb': [
-    # ],
+
 }
--- models/res_partner.py
+++ models/res_partner.py
@@ -7,10 +7,8 @@ class ResPartner(models.Model):
     """Inherits 'res.partner' and adds fields"""
     _inherit = 'res.partner'
 
-    black_list = fields.Boolean(string='Client black listé',
-                                       default='False')
-    created_in = fields.Many2one("res.company", string='Client créer sur',
-                                       required='True')
+    black_list = fields.Boolean(string='Client black listé',default=False)
+    created_in = fields.Many2one("res.company", string='Client créer sur',required=True)
     ccp = fields.Char(string="CCP", required=True, copy=False)
 
     _sql_constraints = [
