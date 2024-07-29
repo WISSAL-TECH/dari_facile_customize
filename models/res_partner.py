d616119326dd6fa85edeba9277cce699fd8afc88
Oumaima MESSAI <messai.oumaima28@gmail.com>
2024-07-29 09:29:18 +0100

    FIXING partner

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class ResPartner(models.Model):
@@ -9,11 +10,15 @@ class ResPartner(models.Model):
 
     black_list = fields.Boolean(string='Client black listé',default=False)
     created_in = fields.Many2one("res.company", string='Client créer sur',required=True,default=lambda self: self.env.company)
-    ccp = fields.Char(string="CCP", required=True, copy=False)
+    ccp = fields.Float(string="CCP", required=True, copy=False)
 
     _sql_constraints = [
-        ('ccp_unique', 'unique(ccp)', 'The CCP must be unique.')
+        ('ccp_unique', 'unique(ccp)', 'Numéro de CCP doit être unique.')
     ]
     arabic_name = fields.Char(string='الاسم')
     arabic_job = fields.Char(string='الوظيفة')
     arabic_birthdate = fields.Date(string='تاريخ الميلاد')
+    def _check_ccp_length(self, vals):
+        ccp = vals.get('ccp')
+        if ccp and len(ccp) != 21:
+            raise UserError("Veuillez vérifier le numéro CCP.")
