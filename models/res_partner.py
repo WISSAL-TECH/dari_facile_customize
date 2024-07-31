2110e9573b161b21f38b5166e1770d65251652e0
Oumaima MESSAI <messai.oumaima28@gmail.com>
2024-07-31 13:03:40 +0100

    adding field

--- models/res_partner.py
+++ models/res_partner.py
@@ -11,12 +11,6 @@ class ResPartner(models.Model):
     black_list = fields.Boolean(string='Client black listé',default=False)
     created_in = fields.Many2one("res.company", string='Client créer sur',required=True,default=lambda self: self.env.company)
     ccp = fields.Integer(string="CCP", required=True, copy=False)
-    start_date = fields.Selection([
-        ('1', 'Le 1er'),
-        ('13', 'Le 13'),
-        ('17', 'Le 17'),
-    ], string="Date de début de prélèvement")
-
     _sql_constraints = [
         ('ccp_unique', 'unique(ccp)', 'Numéro de CCP doit être unique.')
     ]
