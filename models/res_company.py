eef5650cbfc499f0647e1ee72a334ab9fc8e86f1
Oumaima MESSAI <messai.oumaima28@gmail.com>
2024-08-01 09:47:50 +0100

    adding ccp to company

--- models/res_company.py
+++ models/res_company.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class ResCompany(models.Model):
@@ -10,3 +11,15 @@ class ResCompany(models.Model):
     marge_12 = fields.Float(string='Marge sur facilité 12 mois')
     marge_18 = fields.Float("Marge sur facilité 18 mois")
     marge_24 = fields.Float("Marge sur facilité 24 mois")
+    ccp = fields.Char(string="CCP", required=True, copy=False)
+    key_ccp = fields.Char(string="Clé", required=True, copy=False)
+
+    _sql_constraints = [
+        ('ccp_unique', 'unique(ccp)', 'Numéro de CCP doit être unique.')
+    ]
+    @api.constrains('ccp', 'key')
+    def _check_ccp_length(self):
+        if self.ccp and (len(self.ccp) != 8 or not self.ccp.isdigit()):
+            raise UserError("Le numéro de CCP doit comporter exactement 8 chiffres.")
+        if self.key_ccp and (len(self.key_ccp) != 2 or not self.key_ccp.isdigit()):
+            raise UserError("La clé de CCP doit comporter exactement 2 chiffres.")
